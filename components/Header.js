4b5823aafaf4fbae98cc9ce4ce48caeab7a6025d
Tanguy <tan.deshayes@gmail.com>
2021-04-06 16:53:57 +0200

    Scss header and nav create

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                 <img src="" alt=""/>
+                <nav>
+                    <ul>
+                        <li>Accueil</li>
+                        <li>Nos Produits</li>
+                        <li>Notre histoire</li>
+                    </ul>
+                </nav>
+
+                <a href="">Se connecter</a>
+                <h2>Panier</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
-
+import Header from "../components/Header"
 import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Teirlinck</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <Footer />
     </div>
   );
