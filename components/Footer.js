1ef3f3410f4db266900e70bf7832fc02454043b5
Tanguy Deshayes <tan.deshayes@gmail.com>
2021-04-06 18:53:36 +0200

    Add logo.svg, the project work

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import Link from "next/link";
-import styles from "../styles/Footer.module.scss";
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-      <footer className={styles.footer}>
-        <div className={styles.logo}>
+    <div className="container">
+      <footer>
+        <div>
           <img src="/logo-removebg-preview.png" alt="" />
         </div>
         <p>Horaires</p>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.scss";
+
 import Header from "../components/Header"
 import Footer from "../components/Footer";
 
