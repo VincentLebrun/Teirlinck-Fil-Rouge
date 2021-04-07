205751cd2424d9a832b908da379034a50bc2249f
Tanguy Deshayes <tan.deshayes@gmail.com>
2021-04-07 15:23:34 +0200

    Amélioration du header, commencement de la page accueil

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import Header from "../components/Header"
 import Footer from "../components/Footer";
+import { Row, Col } from 'antd';
 
 export default function Home() {
   return (
@@ -13,7 +14,21 @@ export default function Home() {
       </Head>
       <Header />
 
-      <h2>lol</h2>
+      <div className="home-content">
+        <img src="" alt=""/>
+        <Row className="slogan">
+            <h1>Passez commande et venez la récupérer en boucherie</h1>
+            <button>lol</button>
+        </Row>
+      </div>
+
+
+      <Row xs={24} sm={24} justify="center">
+        <Row xs={16} sm={16}>
+          <p>test</p>
+        </Row>
+      </Row>
+
       <Footer />
     </div>
   );
