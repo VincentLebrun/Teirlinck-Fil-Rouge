2deeedf83e6b80b0bb5bbab9d3216dd89930ac4c
Tanguy Deshayes <tan.deshayes@gmail.com>
2021-04-11 01:06:38 +0200

    Commencement du backend, API products fontionne, commencement interface administrateur

---
+++ api/models/product.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    name: String,
+    description: String,
+    image: String,
+    categories: [String],
+    allergenes: [String],
+    price_type: String,
+    price: Number,
+    promotion: Boolean,
+    highlighted: Boolean,
+    available: Boolean
+});
+
+const Product = mongoose.model("product", ProductSchema);
+
+module.exports = Product;
