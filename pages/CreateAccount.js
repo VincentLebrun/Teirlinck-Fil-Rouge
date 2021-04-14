230ae930914548e12ceec9764f8bf5adfda287ca
Vincent-lebrun <lebrunvincent12@gmail.com>
2021-04-14 15:12:22 +0200

    Finished create account page some style to correct and add after inject api

--- pages/CreateAccount.js
+++ pages/CreateAccount.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Row, Col, Form, Input } from "antd";
-//Exemple d'utilisation com!parative de mot de passe 
+import Link from "next/link";
+//Exemple d'utilisation com!parative de mot de passe
 //
 // (async ()=> {
 //   const bcrypt = require("bcryptjs");
@@ -9,7 +10,6 @@ import { Button, Row, Col, Form, Input } from "antd";
 //      const salt= await bcrypt.genSalt(10)
 //      const hash = await bcrypt.hash(text, salt)
 
-     
 //      console.log(hash)
 //      const compare = await bcrypt(text, hash)
 //      console.log(compare)
@@ -26,9 +26,8 @@ const RegistrationForm = () => {
   const onFinish = async (values) => {
     console.log("Received values of form: ", values);
     const pass = values.password;
-    const testCrypt = await  bcrypt.hash(pass, saltRounds);
+    const testCrypt = await bcrypt.hash(pass, saltRounds);
     console.log(testCrypt);
-    
   };
 
   return (
@@ -39,6 +38,7 @@ const RegistrationForm = () => {
             <Col span={24}>
               <img src="/logo.svg" alt="" />
             </Col>
+            <div className="pastille-login"><i class="fi-rr-spinner-alt"></i></div>
             <Col xl={16}>
               <hr />
               <Form
@@ -47,16 +47,65 @@ const RegistrationForm = () => {
                 onFinish={onFinish}
                 scrollToFirstError
               >
-                <Row className="contain" justify="space-around" xl={24}>
+                <div className="input50">
+                  <Row className="contain" justify="space-around" xl={24}>
+                    <Form.Item
+                      className="formInputStyle"
+                      label="Prénom"
+                      name="fisrtName"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Veuillez entrer votre prénom",
+                          whitespace: true,
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                      label="Nom"
+                      className="formInputStyle"
+                      name="name"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Veuillez entrez votre nom",
+                          whitespace: true,
+                        },
+                      ]}
+                    >
+                      <Input />
+                    </Form.Item>
+                  </Row>{" "}
+                </div>
+                <div className="input75">
                   <Form.Item
+                    label="Numéro de téléphone"
                     className="formInputStyle"
-                    label="Prénom"
-                    name="fisrtName"
+                    name="phone"
                     rules={[
                       {
                         required: true,
-                        message: "Veuillez entrer votre prénom",
-                        whitespace: true,
+                        message: "Veuillez saisir un numéro de téléphone",
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item
+                    label="Adresse mail"
+                    className="formInputStyle"
+                    name="email"
+                    rules={[
+                      {
+                        type: "email",
+                        message: "L'e-mail n'est pas valide",
+                      },
+                      {
+                        required: true,
+                        message: "Veuillez taper un e-mail",
                       },
                     ]}
                   >
@@ -64,100 +113,69 @@ const RegistrationForm = () => {
                   </Form.Item>
 
                   <Form.Item
-                    label="Nom"
                     className="formInputStyle"
-                    name="name"
+                    label="Mot de passe"
+                    name="password"
+                    hasFeedback
                     rules={[
                       {
+                        message:
+                          "Le mot de passe doit contenir au minimum 6 caractères et une majuscule",
+                        pattern: /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}/,
                         required: true,
-                        message: "Veuillez entrez votre nom",
-                        whitespace: true,
                       },
                     ]}
                   >
-                    <Input />
+                    {/* <label>Mot de passe"</label> */}
+                    <Input.Password />
                   </Form.Item>
-                </Row>
-                <Form.Item
-                  label="Numéro de téléphone"
-                  className="formInputStyle"
-                  name="phone"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Veuillez saisir un numéro de téléphone",
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  label="E-mail"
-                  className="formInputStyle"
-                  name="email"
-                  rules={[
-                    {
-                      type: "email",
-                      message: "L'e-mail n'est pas valide",
-                    },
-                    {
-                      required: true,
-                      message: "Veuillez taper un e-mail",
-                    },
-                  ]}
-                >
-                  <Input />
-                </Form.Item>
-                <Form.Item
-                  className="formInputStyle"
-                  label="Mot de passe"
-                  name="password"
-                  hasFeedback
-                  rules={[
-                    {
-                      message:
-                        "Le mot de passe doit contenir au minimum 6 caractères et une majuscule",
-                      pattern: /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}/,
-                      required: true,
-                    },
-                  ]}
-                >
-                  {/* <label>Mot de passe"</label> */}
-                  <Input.Password />
-                </Form.Item>
-                <Form.Item
-                  className="formInputStyle"
-                  label="Confirmez le mot de passe"
-                  name="confirm"
-                  dependencies={["password"]}
-                  hasFeedback
-                  rules={[
-                    {
-                      required: true,
-                      message: "Confirmez votre mot de passe",
-                    },
-                    ({ getFieldValue }) => ({
-                      validator(_, value) {
-                        if (!value || getFieldValue("password") === value) {
-                          return Promise.resolve();
-                        }
-                        return Promise.reject(
-                          new Error("Les 2 mot de passe ne correspondent pas ")
-                        );
+
+                  <Form.Item
+                    className="formInputStyle"
+                    label="Confirmez le mot de passe"
+                    name="confirm"
+                    dependencies={["password"]}
+                    hasFeedback
+                    rules={[
+                      {
+                        required: true,
+                        message: "Confirmez votre mot de passe",
                       },
-                    }),
-                  ]}
-                >
-                  {/* <label>Confirmez le mot de passe</label> */}
-                  <Input.Password />
-                </Form.Item>
+                      ({ getFieldValue }) => ({
+                        validator(_, value) {
+                          if (!value || getFieldValue("password") === value) {
+                            return Promise.resolve();
+                          }
+                          return Promise.reject(
+                            new Error(
+                              "Les 2 mot de passe ne correspondent pas "
+                            )
+                          );
+                        },
+                      }),
+                    ]}
+                  >
+                    {/* <label>Confirmez le mot de passe</label> */}
+                    <Input.Password />
+                  </Form.Item>
+                </div>
                 <Form.Item>
                   <Button className="button" htmlType="submit">
-                    Créer un compte
+                    Créer votre compte
                   </Button>
                 </Form.Item>
+                <p>
+                  <i class="fi-rr-info"></i>Avant de pouvoir passer votre
+                  commande il faudra faire valider votre compte en boucherie
+                </p>
                 <hr />
               </Form>
+              <h2>Vous avez déjà un compte?</h2>
+              <Link href={"/hello.js"}>
+                <Button className="button" htmlType="submit">
+                  Se connecter
+                </Button>
+              </Link>
             </Col>
           </Row>
         </Col>
