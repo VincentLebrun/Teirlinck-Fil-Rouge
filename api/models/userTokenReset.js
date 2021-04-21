600841ff2899af986fcfd01f6e2d2c7431844396
Vincent-lebrun <lebrunvincent12@gmail.com>
2021-04-21 13:44:49 +0200

    Add reset password page and send mail to user not finished

---
+++ api/models/userTokenReset.js
@@ -0,0 +1,11 @@
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const UserToken = new Schema({
+//     resetToken:String,
+//     expireToken:Date
+// });
+
+
+// const UserToken = mongoose.model("user", UserToken);
