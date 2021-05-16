d1593445ac1d3f08998aefc9275a12fd149ae3b6
Vincent-lebrun <lebrunvincent12@gmail.com>
2021-05-16 14:59:05 +0200

    Test and commit on develop to make a new branch

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,104 +3,102 @@ const UserController = require("../controllers/user");
 const OrderController = require("../controllers/order");
 const checkAuth = require("../middleware/check-auth");
 
-module.exports = server => {
+module.exports = (server) => {
+  // PRODUITS
+  server.get("/products", (req, res) => {
+    ProductController.getAll(req, res);
+  });
 
-    // PRODUITS
-    server.get("/products", (req, res) => {
-        ProductController.getAll(req, res);
+  server.get("/products/:id", (req, res) => {
+    ProductController.get(req, res);
+  });
+
+  server.post("/products", checkAuth, async (req, res) => {
+    ProductController.create(req, res);
+  });
+
+  server.put("/products", checkAuth, async (req, res) => {
+    ProductController.update(req, res);
+  });
+
+  server.delete("/products", checkAuth, (req, res) => {
+    ProductController.delete(req, res);
+  });
+
+  // UTILISATEURS
+
+  server.get("/users", checkAuth, (req, res, next) => {
+    UserController.getAll(req, res);
+  });
+
+  // server.get("/users/:id", (req, res) => {
+  //     UserController.get(req, res);
+  // });
+
+  server.post("/users", async (req, res) => {
+    UserController.create(req, res);
+  });
+
+  server.put("/users", checkAuth, async (req, res) => {
+    UserController.update(req, res);
+  });
+
+  server.delete("/users", checkAuth, (req, res) => {
+    UserController.delete(req, res);
+  });
+
+  // COMMANDES
+
+  server.get("/orders", checkAuth, (req, res) => {
+    OrderController.getAll(req, res);
+  });
+
+  server.get("/orders/:id", (req, res) => {
+    OrderController.get(req, res);
+  });
+
+  server.post("/orders", checkAuth, async (req, res) => {
+    OrderController.create(req, res);
+  });
+
+  server.put("/orders", checkAuth, async (req, res) => {
+    OrderController.update(req, res);
+  });
+
+  server.delete("/orders", checkAuth, (req, res) => {
+    OrderController.delete(req, res);
+  });
+
+  //LOGIN
+
+  server.post("/login", async (req, res) => {
+    UserController.login(req, res);
+  });
+
+  server.post("/resetpassword", (req, res) => {
+    crypto.randomBytes(32, (err, buffer) => {
+      if (err) {
+        console.log(err);
+      }
+      const token = buffer.toString("hex");
+      User.findOne({ email: req.body.email }).then((user) => {
+        if (!user) {
+          return res.status(422).json({ error: "Cet e-mail n'existe pas " });
+        }
+        user.resetToken = token;
+        user.expireToken = Date.now() + 3600000;
+        user.save().then((result) => {
+          transporter.sendMail({
+            to: user.email,
+            from: "no-reply",
+            subject: "password reset",
+            html: `<p>Vous avez sollicité un changement de mot de passe</p>
+                          <h5>Cliquez sur ce <a href="http://localhost:3000/resetpassword${token}" >lien</a> pour le changer </h5>`,
+          });
+          res.json({ message: "Validez votre e-mail" });
+        });
+        //
+      });
     });
-
-    server.get("/products/:id", (req, res) => {
-        ProductController.get(req, res);
-    });
-
-    server.post("/products", checkAuth,  async (req, res) => {
-        ProductController.create(req, res);
-    });
-
-    server.put("/products", checkAuth, async (req, res) => {
-        ProductController.update(req, res);
-    });
-
-    server.delete("/products",checkAuth, (req, res) => {
-        ProductController.delete(req, res);
-    });
-
-    // UTILISATEURS
-
-    server.get("/users", checkAuth, (req, res, next) => {
-        UserController.getAll(req, res);
-    });
-
-    // server.get("/users/:id", (req, res) => {
-    //     UserController.get(req, res);
-    // });
-
-    server.post("/users", async (req, res) => {
-        UserController.create(req, res);
-    });
-
-    server.put("/users", checkAuth, async (req, res) => {
-        UserController.update(req, res);
-    });
-
-    server.delete("/users", checkAuth, (req, res) => {
-        UserController.delete(req, res);
-    });
-
-    // COMMANDES 
-
-    server.get("/orders", checkAuth, (req, res) => {
-        OrderController.getAll(req, res);
-    });
-
-    server.get("/orders/:id", (req, res) => {
-        OrderController.get(req, res);
-    });
-
-    server.post("/orders", checkAuth, async (req, res) => {
-        OrderController.create(req, res);
-    });
-
-    server.put("/orders", checkAuth, async (req, res) => {
-        OrderController.update(req, res);
-    });
-
-    server.delete("/orders", checkAuth, (req, res) => {
-        OrderController.delete(req, res);
-    });
-
-    //LOGIN
-
-    server.post("/login", async (req, res) => {
-        UserController.login(req, res);
-    });
-}
-
-    // server.post('reset-password', (req,res)=>{
-    //     crypto.randomBytes(32,(err,buffer)=>{
-    //         if(err){
-    //             console.log(err)
-    //         }
-    //         const token = buffer.toString("hex")
-    //         User.findOne({email:req.body.email}).then(user=>{
-    //             if(!user){
-    //                 return res.status(422).json({error:"Cet e-mail n'existe pas "})
-    //             }
-    //             user.resetToken = token
-    //             user.expireToken= Date.now() + 3600000
-    //             user.save().then((result)=>{
-    //                 transporter.sendMail({
-    //                     to:user.email,
-    //                     from:"no-reply",
-    //                     subject:"password reset",
-    //                     html: `<p>Vous avez sollicité un changement de mot de passe</p>
-    //                     <h5>Cliquez sur ce <a href="http://localhost:3000/resetpassword${token}" >lien</a> pour le changer </h5>`
-    //                 })
-    //                 res.json({message:"Validez votre e-mail"})
-    //             })
-    //             //
-    //         })
-    //     })
-    // })
-// }
+  });
+};
--- pages/resetpassword.js
+++ pages/resetpassword.js
@@ -1,109 +1,105 @@
-// import { Button, Row, Col, Form, Input } from "antd";
-// import Link from "next/link";
-// const bcrypt = require("bcryptjs");
-// const saltRounds = 10;
+import { Button, Row, Col, Form, Input } from "antd";
+import Link from "next/link";
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
 
-// const NewPassword = () => {
-//   const [form] = Form.useForm();
+const NewPassword = () => {
+  const [form] = Form.useForm();
 
-//   const onFinish = async (values) => {
-//     const pass = values.password;
-//     const testCrypt = await bcrypt.hash(pass, saltRounds);
-//     const resetPass = {
-     
-      
-//       password: testCrypt,
-//     };
+  const onFinish = async (values) => {
+    const pass = values.password;
+    const testCrypt = await bcrypt.hash(pass, saltRounds);
+    const resetPass = {
+      password: testCrypt,
+    };
 
-//     await fetch(process.env.NEXT_PUBLIC_API_USERS, {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(resetPass),
-//     }).catch((error) => console.log(error));
-//   };
+    await fetch(process.env.NEXT_PUBLIC_API_USERS, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(resetPass),
+    }).catch((error) => console.log(error));
+  };
 
-// return (
-//   <main>
-//     <Row justify="center" align="middle" className="container1">
-//       <Col xs={22} xl={16}>
-//         <Row className="black-container" justify="center" align="middle">
-//           <Col span={24}>
-//             <Link href="/">
-//               <img src="/logo.svg" alt="" />
-//             </Link>
-//           </Col>
-//           <div className="pastille-login">
-//             <i class="fi-rr-spinner-alt"></i>
-//           </div>
-//           <Col xl={16}>
-//             <hr />
-//             <Form
-//               form={form}
-//               name="register"
-//               onFinish={onFinish}
-//               scrollToFirstError
-//             >
-//               <Row className="contain" justify="space-around" xl={24}></Row>
-             
-//               <Form.Item
-//                 className="formInputStyle"
-//                 label="Nouveau mot de passe"
-//                 name="password"
-//                 hasFeedback
-//                 rules={[
-//                   {
-//                     message:
-//                       "Le mot de passe doit contenir au minimum 8 caractères une majuscule et un numéro",
-//                     pattern: /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}/,
-//                     required: true,
-//                   },
-//                 ]}
-//               >
-//                 <Input.Password />
-//               </Form.Item>
-//               <Form.Item
-//                     className="formInputStyle"
-//                     label="Confirmez le mot de passe"
-//                     name="confirm"
-//                     dependencies={["password"]}
-//                     hasFeedback
-//                     rules={[
-//                       {
-//                         required: true,
-//                         message: "Confirmez votre mot de passe",
-//                       },
-//                       ({ getFieldValue }) => ({
-//                         validator(_, value) {
-//                           if (!value || getFieldValue("password") === value) {
-//                             return Promise.resolve();
-//                           }
-//                           return Promise.reject(
-//                             new Error(
-//                               "Les 2 mot de passe ne correspondent pas "
-//                             )
-//                           );
-//                         },
-//                       }),
-//                     ]}
-//                   >
-                   
-//                     <Input.Password />
-//                   </Form.Item>
+  return (
+    <main>
+      <Row justify="center" align="middle" className="container1">
+        <Col xs={22} xl={16}>
+          <Row className="black-container" justify="center" align="middle">
+            <Col span={24}>
+              <Link href="/">
+                <img src="/logo.svg" alt="" />
+              </Link>
+            </Col>
+            <div className="pastille-login">
+              <i class="fi-rr-spinner-alt"></i>
+            </div>
+            <Col xl={16}>
+              <hr />
+              <Form
+                form={form}
+                name="register"
+                onFinish={onFinish}
+                scrollToFirstError
+              >
+                <Row className="contain" justify="space-around" xl={24}></Row>
 
-//               <Form.Item>
-//                 <Button className="button" htmlType="submit">
-//                   Changement de mot de passe
-//                 </Button>
-//               </Form.Item>
-//             </Form>
-//           </Col>
-//         </Row>
-//       </Col>
-//     </Row>
-//   </main>
-// );};
+                <Form.Item
+                  className="formInputStyle"
+                  label="Nouveau mot de passe"
+                  name="password"
+                  hasFeedback
+                  rules={[
+                    {
+                      message:
+                        "Le mot de passe doit contenir au minimum 8 caractères une majuscule et un numéro",
+                      pattern: /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}/,
+                      required: true,
+                    },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+                <Form.Item
+                  className="formInputStyle"
+                  label="Confirmez le mot de passe"
+                  name="confirm"
+                  dependencies={["password"]}
+                  hasFeedback
+                  rules={[
+                    {
+                      required: true,
+                      message: "Confirmez votre mot de passe",
+                    },
+                    ({ getFieldValue }) => ({
+                      validator(_, value) {
+                        if (!value || getFieldValue("password") === value) {
+                          return Promise.resolve();
+                        }
+                        return Promise.reject(
+                          new Error("Les 2 mot de passe ne correspondent pas ")
+                        );
+                      },
+                    }),
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
 
-// export default NewPassword;
+                <Form.Item>
+                  <Button className="button" htmlType="submit">
+                    Changement de mot de passe
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </main>
+  );
+};
+
+export default NewPassword;
