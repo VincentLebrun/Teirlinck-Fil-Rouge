20f2939ac7d71ae0fa11d6586b1571e9ffe28db8
Timothé <timothegauquelin@gmail.com>
2021-09-08 12:12:24 +0200

    Ajout d'un bouton 'Voir Moins' qui ne s'affiche qu'à partir du premier click du bouton 'Voir Plus'

--- components/Seeless.js
+++ components/Seeless.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Seeless = ({ action, display }) => {
     return (
-        // <button className="seemore" onClick={action} >Voir Moins <i className="fi-rr-angle-up arrow-up"></i></button>
         <button className={display <= 4 ? "notDisplaying" : "seemore"} onClick={action} >Voir Moins <i className="fi-rr-angle-up arrow-up"></i></button>
     )
 }
