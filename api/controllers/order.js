80b0db44e8898b2bc726c3404b8a4aaabe771778
Tanguy <tan.deshayes@gmail.com>
2021-10-04 11:38:06 +0200

    Mail creation de compte, commencement du mail passage de commande

--- api/controllers/order.js
+++ api/controllers/order.js
@@ -1,4 +1,30 @@
 const Order = require("../models/order");
+const User = require("../models/user");
+const nodemailer = require('nodemailer');
+var handlebars = require('handlebars');
+let fs = require('fs');
+
+let readHTMLFile = function (path, callback) {
+    fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+        if (err) {
+            throw err;
+            callback(err);
+        }
+        else {
+            callback(null, html);
+        }
+    });
+};
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: 'teirlinck.boucherie@gmail.com',
+        pass: 'jxqikpgtlupqlqyd'
+    }
+});
+
 module.exports = {
     getAll(req, res) {
         Order.find().then(orders => {
@@ -17,18 +43,46 @@ module.exports = {
             numero: req.body.numero,
             products: req.body.products,
             // req.body.cart.items,
-            total:req.body.total,
+            total: req.body.total,
             // req.body.cart.total,
             date: req.body.date,
             user_id: req.body.user_id,
             user_firstname: req.body.user_firstname,
-            user_lastname:req.body.user_lastname,
-            user_phone:req.body.user_phone,
+            user_lastname: req.body.user_lastname,
+            user_phone: req.body.user_phone,
             delivered: req.body.delivered,
             ready: req.body.ready
         });
         order.save().then(() => {
-            res.send({ result: `Création de la commande n° ${order.numero}` });
+            User.findById(req.body.user_id).then(user => {
+                readHTMLFile(process.cwd() + '/views/createAccountTemplate.html', function (err, html) {
+                    let template = handlebars.compile(html);
+                    const tableau = req.body.products.map(item => { `<p>hello</p>` })
+                    let replacements = {
+                        firstname: req.body.user_firstname,
+                        tableau
+                    }
+                    let htmlToSend = template(replacements);
+                    transporter.sendMail({
+                        from: `Boucherie Teirlinck <teirlinck.boucherie@gmail.com>`,
+                        to: "tan.deshayes@gmail.com",
+                        // user.mail
+                        subject: 'Votre commande sur boucherie-teirlinck.fr',
+                        html: htmlToSend,
+                        attachments: [
+                            {
+                                filename: 'teirlinck.png',
+                                path: process.cwd() + '/views/images/teirlinck.png',
+                                cid: "image"
+                            }
+                        ]
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                })
+                res.send({ result: `Création de la commande n° ${order.numero}` });
+            })
+
         });
     },
     update(req, res) {
