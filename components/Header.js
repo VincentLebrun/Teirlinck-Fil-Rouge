d7b772b61edc3a0c8074895cb6bab9bab8942d83
Tanguy <tan.deshayes@gmail.com>
2021-11-22 11:45:35 +0100

    Animation header et productElement

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,13 @@ import { React, useState } from 'react'
 import Link from 'next/link'
 import { Row, Col } from 'antd';
 import { Drawer, Button } from 'antd';
+import { WindowsFilled } from '@ant-design/icons';
 // import { AiOutlineMenuFold } from "react-icons/ai";
 
 const Header = (props) => {
 
     const [visible, setVisible] = useState(false);
+    const [scroll, setScroll] = useState(scrollY);
     const showDrawer = () => {
         setVisible(true);
     };
@@ -14,9 +16,14 @@ const Header = (props) => {
         setVisible(false);
     };
 
+    window.onscroll = function () {
+        setScroll(scrollY)
+    }
+
+
     return (
         <header>
-            <Row justify="center" align="middle" className="header">
+            <Row justify="center" align="middle" className={`header ${scroll > 200 ? "headerHeightDown" : "headerHeightTop"}`}>
                 <Col xs={21} sm={20} lg={16} >
                     <Row justify="space-between" align="middle">
 
@@ -53,9 +60,9 @@ const Header = (props) => {
 
                                     <nav>
                                         <ul>
-                                        <Link href="/"><li>Accueil</li></Link>
-                                        <Link href="/products/"><li>Nos produits</li></Link>
-                                        <Link href="/notrehistoire"><li>Notre histoire</li></Link>
+                                            <Link href="/"><li>Accueil</li></Link>
+                                            <Link href="/products/"><li>Nos produits</li></Link>
+                                            <Link href="/notrehistoire"><li>Notre histoire</li></Link>
                                         </ul>
                                     </nav>
                                 </Drawer>
